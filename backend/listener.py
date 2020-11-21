bcf24ca02430fe2179eab02b7ee0bbf3ec6bd2d8
tanishqvyas <tanishqvyas069@gmail.com>
2020-11-21 19:43:33 +0530

    maded worker threads = 8 (num of cores)

--- backend/listener.py
+++ backend/listener.py
@@ -2,17 +2,14 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 # Create a local StreamingContext with two working thread and batch interval of 1 second
-sc = SparkContext("local[2]", appName="FantasyLeagueAnalysis")
+sc = SparkContext(master="local[8]", appName="FantasyLeagueAnalysis")
 ssc = StreamingContext(sc, 1)
 
 # Create an input DStream that will connect to hostname:port, like localhost:9999
 lines = ssc.socketTextStream("localhost", 6100)
 
-words = lines.map(lambda line: line)
 
-words.pprint()
-
-
+lines.pprint()
 
 
 # Start the computation
