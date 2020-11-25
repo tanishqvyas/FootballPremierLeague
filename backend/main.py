09170af202dc6cbe70d8d094aa6e4c4a78c10674
tanishqvyas <tanishqvyas069@gmail.com>
2020-11-26 01:52:41 +0530

    cleaned the code

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,23 @@
 #!usr/bin/python3
 
+# Standard Imports
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SQLContext
 from pyspark.sql.session import SparkSession
 import pyspark.sql.types as tp
+
+import json
 import os
+import socket
+import sys
+import threading
 
-#import listener
+# Custom Imports
 from utils.helper import *
-import socket,sys,json,threading
 
 
-def stream_data():
 
-	# Create a local StreamingContext with two working thread and batch interval of 1 second
-	sc = SparkContext(master="local[2]", appName="FantasyLeagueAnalysis")
-	ssc = StreamingContext(sc, 5)
-
-	# Create an input DStream that will connect to hostname:port, like localhost:9999
-	lines = ssc.socketTextStream("localhost", 6100)
-	calc_metrics(lines)
-
-	# Start the computation
-	ssc.start()
-	# Wait for the computation to terminate
-	ssc.awaitTermination()  
-
-	ssc.stop(stopSparkContext=False, stopGraceFully=True)
-
-
-def calc_metrics(lines):
-	#global lines
-	sc = SparkContext(master="local", appName="FPL3")
-	match=lines.first()
-	#events=
-	print(match)
-	#print(events)
 
 
 
@@ -102,6 +83,7 @@ if __name__ =="__main__":
 	lines.pprint()
 
 	# Structure the data and extract relevant data
+	# To-DO
 	
 
 	# Compute  the Metrics
