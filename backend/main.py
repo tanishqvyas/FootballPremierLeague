6361b4fa10c39b7c915db6d90af57db74ee0e691
tanishqvyas <tanishqvyas069@gmail.com>
2020-11-26 01:07:54 +0530

    Added Player, Teams Schema

--- backend/main.py
+++ backend/main.py
@@ -3,30 +3,17 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SQLContext
+from pyspark.sql.session import SparkSession
+import pyspark.sql.types as tp
+import os
+
 #import listener
 from utils.helper import *
 import socket,sys,json,threading
 
-players=None
-teams=None
-def read_data():
-	global players
-	global teams
-	sc = SparkContext("local", "FPL2")
-	sql_context = SQLContext(sc)
 
-	players = sql_context.read.csv("../data/players.csv", header=True)
-	teams = sql_context.read.csv("../data/teams.csv", header=True)
-	
-	'''
-	for i in df1.collect():
-		print(i)
-	'''
-	#sc.stop()
-	#sql_context.stop()
-lines=None
 def stream_data():
-	global lines
+
 	# Create a local StreamingContext with two working thread and batch interval of 1 second
 	sc = SparkContext(master="local[2]", appName="FantasyLeagueAnalysis")
 	ssc = StreamingContext(sc, 5)
@@ -34,13 +21,15 @@ def stream_data():
 	# Create an input DStream that will connect to hostname:port, like localhost:9999
 	lines = ssc.socketTextStream("localhost", 6100)
 	calc_metrics(lines)
-	#lines.pprint()
+
 	# Start the computation
 	ssc.start()
 	# Wait for the computation to terminate
 	ssc.awaitTermination()  
 
 	ssc.stop(stopSparkContext=False, stopGraceFully=True)
+
+
 def calc_metrics(lines):
 	#global lines
 	sc = SparkContext(master="local", appName="FPL3")
@@ -48,10 +37,49 @@ def calc_metrics(lines):
 	#events=
 	print(match)
 	#print(events)
+
+
+
 if __name__ =="__main__":
-	read_data()
-	stream_data()
-	#t1=threading.Thread(target=stream_data)
-	#t2=threading.Thread(target=calc_metrics)
-	#t1.start()
-	#t2.start()
+
+	# Data Paths
+	Player_CSV_Path = os.path.join("..", "data", "players.csv")
+	Teams_CSV_Path = os.path.join("..", "data", "teams.csv")
+
+	# Initializing spark session : Unified entry point
+	sc = SparkContext(appName="FantasyPremierLeague").getOrCreate()
+	ssc = SparkSession(sc)
+
+	# Player schema
+	Players_schema = tp.StructType([
+	tp.StructField(name= 'name',   			dataType= tp.StringType(),   nullable= False),
+	tp.StructField(name= 'birthArea',   	dataType= tp.StringType(),   nullable= False),
+	tp.StructField(name= 'birthDate',   	dataType= tp.TimestampType(),   nullable= False),
+	tp.StructField(name= 'foot',   			dataType= tp.StringType(),   nullable= False),
+	tp.StructField(name= 'role',   			dataType= tp.StringType(),   nullable= False),
+	tp.StructField(name= 'height',   		dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'passportArea',   	dataType= tp.StringType(),   nullable= False),
+	tp.StructField(name= 'weight',   		dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'Id', 				dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'numFouls', 		dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'numGoals', 		dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'numOwnGoals', 	dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'passAccuracy', 	dataType= tp.IntegerType(),  nullable= False),
+	tp.StructField(name= 'shotsOnTarget', 	dataType= tp.IntegerType(),  nullable= False)
+	])
+
+	# Teams Schema
+	Teams_schema = tp.StructType([
+	tp.StructField(name= 'name', 			dataType= tp.StringType(),  nullable= False),
+	tp.StructField(name= 'Id', 				dataType= tp.IntegerType(),  nullable= False)
+	])
+
+	# Load the Players and Teams data from CSV file
+	Player_RDD = ssc.read.csv(Player_CSV_Path, schema=Players_schema, header=True)
+	Teams_RDD = ssc.read.csv(Teams_CSV_Path, schema=Teams_schema, header=True)
+
+	# Print the head
+	print(Player_RDD.show(5))
+	print(Teams_RDD.show(5))
+
+
