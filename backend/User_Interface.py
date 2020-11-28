146078691c819a773f0435dead2ad7dff8d61f95
tanishqvyas <tanishqvyas069@gmail.com>
2020-11-28 16:39:10 +0530

    Initialized response dictionaries

--- backend/User_Interface.py
+++ backend/User_Interface.py
@@ -6,17 +6,95 @@ import os
 
 # Function to handle request 1
 def handle_request_one(request):
-    pass
+    
+    # Invalid request handler variables
+    msg = "Invalid Request"
+    valid_request = True
+
+
+    # Initializing the response dict
+    response = dict()
+    response["team1"] = dict()
+    response["team2"] = dict()
+    response["team1"]["name"] = "Name 1"
+    response["team2"]["name"] = "Name 2"
+    response["team1"]["winning chance"] = 0
+    response["team2"]["winning chance"] = 0
+
+    # Process the request
+
+
+
+    # Returning the response
+    if valid_request:
+        return response
+    else:
+        response = dict()
+        response["msg"] = msg
+        return response
 
 
 # Function to handle request 2
 def handle_request_two(request):
-    pass
+    
+    # Invalid request handler variables
+    msg = "Invalid Request"
+    valid_request = True
+
+    # Initializing the response dict
+    response = dict()
+
+    fields_list = ["name", "birthArea", "birthDate", "foot", "role", "height", "passportArea", "weight", "fouls", "goals", "own_goals", "percent_pass_accuracy", "percent_shots_on_target"]
+    for field in fields_list:
+        response[field] = "sample data"
+
+
+    # Process the request
+
+
+
+    # Returning the response
+    if valid_request:
+        return response
+    else:
+        response = dict()
+        response["msg"] = msg
+        return response
 
 
 # Function to handle request 3
 def handle_request_three(request):
-    pass
+    
+    # Invalid request handler variables
+    msg = "Invalid Request"
+    valid_request = True
+
+    # Initializing the response dict
+    response = dict()
+    response["yellow_cards"] = []
+    response["red_cards"] = []
+
+    # String Fields and Number fields
+    fields_list = ["date", "duration", "winner", "venue", "gameweek"]
+    for field in fields_list:
+        response[field] = "sample data"
+
+    # List of dictionaries
+    response["goals"] = []
+    response["own_goals"] = []
+
+
+    # Process the request
+
+
+
+    # Returning the response
+    if valid_request:
+        return response
+    else:
+        response = dict()
+        response["msg"] = msg
+        return response
 
 
 
